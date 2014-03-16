d97ed95449225b16049a2425047087925e0bd47c
Pierre-Yves LEBECQ <py.lebecq@gmail.com>
2014-03-16 18:02:30 +0100

    Added Out of this World.

---
+++ Out of this World/Out of this World/PYLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PYLAppDelegate.h
+//  Out of this World
+//
+//  Created by Pierre-Yves LEBECQ on 16/03/2014.
+//  Copyright (c) 2014 Pierre-Yves LEBECQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PYLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of this World/Out of this World/PYLOuterSpaceTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  PYLOuterSpaceTableViewController.h
+//  Out of this World
+//
+//  Created by Pierre-Yves LEBECQ on 16/03/2014.
+//  Copyright (c) 2014 Pierre-Yves LEBECQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PYLOuterSpaceTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *planets;
+
+@end
