88da4caa30338ada2ca822803a9ab76c8064b0ce
Pierre-Yves LEBECQ <py.lebecq@gmail.com>
2014-06-09 18:45:07 +0200

    [Thousand Words] Added the default storyboard.

---
+++ Thousand Words/Thousand Words/PYLAlbumTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PYLAlbumTableViewController.h
+//  Thousand Words
+//
+//  Created by Pierre-Yves LEBECQ on 09/06/2014.
+//  Copyright (c) 2014 Pierre-Yves LEBECQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PYLAlbumTableViewController : UITableViewController
+
+@end
