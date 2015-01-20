f856d3b7428ab2e510ee53b94946db35fc15a257
Pierre-Yves Lebecq <py.lebecq@gmail.com>
2015-01-20 11:39:03 +0100

    [MatchedUp] Reworked UI: TabbarViewController was removed

---
+++ MatchedUp/MatchedUp/Controller/HomeViewController.m
@@ -0,0 +1,71 @@
+//
+//  HomeViewController.m
+//  MatchedUp
+//
+//  Created by Pierre-Yves LEBECQ on 20/01/2015.
+//  Copyright (c) 2015 Pierre-Yves Lebecq. All rights reserved.
+//
+
+#import "HomeViewController.h"
+
+@interface HomeViewController ()
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *chatBarButtonItem;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *settingsBarButtonItem;
+@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
+@property (weak, nonatomic) IBOutlet UILabel *firstNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *ageLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tagLineLabel;
+@property (weak, nonatomic) IBOutlet UIButton *likeButton;
+@property (weak, nonatomic) IBOutlet UIButton *infoButton;
+@property (weak, nonatomic) IBOutlet UIButton *dislikeButton;
+
+@end
+
+@implementation HomeViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark - IBActions
+
+- (IBAction)chatBarbuttonItemPressed:(UIBarButtonItem *)sender
+{
+}
+
+- (IBAction)settingsBarButtonItemPressed:(UIBarButtonItem *)sender
+{
+}
+
+- (IBAction)likeButtonPressed:(UIButton *)sender
+{
+}
+
+- (IBAction)dislikeButtonPressed:(UIButton *)sender
+{
+}
+
+- (IBAction)infoButtonPressed:(UIButton *)sender
+{
+}
+
+@end
