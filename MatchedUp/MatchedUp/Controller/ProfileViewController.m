73bdb4f5e8e7083a0457c3587ec7d3a2d44eb35c
Pierre-Yves Lebecq <py.lebecq@gmail.com>
2015-01-20 12:24:38 +0100

    [MatchedUp] Added profile controller

---
+++ MatchedUp/MatchedUp/Controller/ProfileViewController.m
@@ -0,0 +1,43 @@
+//
+//  ProfileViewController.m
+//  MatchedUp
+//
+//  Created by Pierre-Yves LEBECQ on 20/01/2015.
+//  Copyright (c) 2015 Pierre-Yves Lebecq. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+
+@interface ProfileViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *profilePictureImageView;
+@property (weak, nonatomic) IBOutlet UILabel *locationLabel;
+@property (weak, nonatomic) IBOutlet UILabel *ageLabel;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tagLineLabel;
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
