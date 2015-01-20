07ae25353e185ae74bc04eef555f2c654fff576e
Pierre-Yves Lebecq <py.lebecq@gmail.com>
2015-01-20 12:16:28 +0100

    [MatchedUp] Added settings and edit profile controllers

---
+++ MatchedUp/MatchedUp/Controller/EditProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditProfileViewController.h
+//  MatchedUp
+//
+//  Created by Pierre-Yves LEBECQ on 20/01/2015.
+//  Copyright (c) 2015 Pierre-Yves Lebecq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditProfileViewController : UIViewController
+
+@end
